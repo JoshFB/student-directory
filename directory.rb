478eba0723d6c5f0888310ffdfd4029f39f93b72
Josh <jfb1@hotmail.co.uk>
2014-04-03 18:42:17 +0100

    arrays

--- directory.rb
+++ directory.rb
@@ -1,25 +1,52 @@
-student_count = 9
-# First, we print the list of students
-puts "The students of my cohort at Makers Academy"
-puts "Josh Fail-Brown"
-puts "Steve Musgrave"
-puts "Sroop Sunar"
-puts "Colin Marshall"
-puts "Josh Fail-Brown"
-puts "Louise Lai"
-puts "Robin Doble"
-puts "Alex Wong"
-puts "Scott Dimmock"
-puts "Muhanad Al-Rubaiee"
-puts "Shelley Hope"
-puts "Will Hall"
-puts "Oliver Delevingne"
-puts "Nico"
-puts "Apostolis"
-puts "Stefania"
-puts "Robert Leon"
-puts "Emma Williams"
-puts "Joey Wolf"
-puts "Julie Walker"
+
+# lets put all the students into an array
+student = [
+"The students of my cohort at Makers Academy",
+ "Steve Musgrave",
+ "Sroop Sunar",
+ "Colin Marshall",
+ "Josh Fail-Brown",
+ "Louise Lai",
+ "Robin Doble",
+ "Alex Wong",
+ "Scott Dimmock",
+ "Muhanad Al-Rubaiee",
+ "Shelley Hope",
+ "Will Hall",
+ "Oliver Delevingne",
+ "Nico",
+ "Apostolis",
+ "Stefania",
+ "Robert Leon",
+ "Emma Williams",
+ "Joey Wolf",
+ "Julie Walker",
+ "Daniel le Dosquet",
+]
+
+# and then print them
+puts "The student of my cohort at Makers Academy"
+puts "------------"
+puts student [0]
+puts student [1]
+puts student [2]
+puts student [3]
+puts student [4]
+puts student [5]
+puts student [6]
+puts student [7]
+puts student [8]
+puts student [9]
+puts student [10]
+puts student [11]
+puts student [12]
+puts student [13]
+puts student [14]
+puts student [15]
+puts student [16]
+puts student [17]
+puts student [18]
+puts student [19]
+puts student [20]
 # Finally, we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{student.length} great students"
