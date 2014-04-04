d787b0d3a40d7e5e4d5016b359904c811268c3eb
Josh <jfb1@hotmail.co.uk>
2014-04-04 09:33:46 +0100

    iteration

--- directory.rb
+++ directory.rb
@@ -1,6 +1,8 @@
 
 # lets put all the students into an array
-student = [
+
+
+students = [
 "The students of my cohort at Makers Academy",
  "Steve Musgrave",
  "Sroop Sunar",
@@ -24,29 +26,16 @@ student = [
  "Daniel le Dosquet",
 ]
 
+students.each do |student|
+	puts student
+end
+
+#Finally we print the total
+
+puts "Overalo, we have #{students.length} great students"
+
 # and then print them
-puts "The student of my cohort at Makers Academy"
-puts "------------"
-puts student [0]
-puts student [1]
-puts student [2]
-puts student [3]
-puts student [4]
-puts student [5]
-puts student [6]
-puts student [7]
-puts student [8]
-puts student [9]
-puts student [10]
-puts student [11]
-puts student [12]
-puts student [13]
-puts student [14]
-puts student [15]
-puts student [16]
-puts student [17]
-puts student [18]
-puts student [19]
-puts student [20]
+
+
 # Finally, we print the total
-puts "Overall, we have #{student.length} great students"
+
